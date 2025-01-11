d6b6940e2e0716114d338a0bbf749973aad0a4a2
Himanshu Khairajani <46777429+Khairajani@users.noreply.github.com>
2025-01-12 00:45:23 +0530

    Time: 35 ms (6.41%), Space: 18.9 MB (14.31%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,10 +1,13 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        difference_map = {}
-        nums = numbers
-        for i in range(len(nums)):
-            difference_value_index = difference_map.get(nums[i])
-            if difference_value_index is None:
-                difference_map[target-nums[i]] = i
+        i=0
+        j=len(numbers)-1
+
+        while i<j:
+            print(i,j)
+            if numbers[i]+numbers[j]==target:
+                return [i+1,j+1]
+            elif numbers[i]+numbers[j]<target:
+                i+=1
             else:
-                return [difference_value_index+1,i+1]
+                j-=1
