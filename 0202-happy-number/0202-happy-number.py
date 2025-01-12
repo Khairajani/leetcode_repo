124a177a1127e688186bc9aec6c79d47e46334c9
Himanshu Khairajani <46777429+Khairajani@users.noreply.github.com>
2025-01-12 18:05:06 +0530

    Time: 5 ms (7.12%), Space: 17.7 MB (26.59%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,16 @@
+class Solution:
+        
+    def isHappy(self, n: int) -> bool:
+        flag = True
+        number_set = {}
+        while flag:
+            n = sum([int(element)**2 for element in str(n)])
+            if n==1:
+                flag=True
+                break
+            elif number_set.get(n)!=None:
+                flag=False
+                break
+            else:
+                number_set[n]=1
+        return flag
