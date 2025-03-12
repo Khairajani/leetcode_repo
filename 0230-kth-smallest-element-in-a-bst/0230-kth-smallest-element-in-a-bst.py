e04558f2900df5d990cdeddba9ae5d4f8e5277bc
Himanshu Khairajani <46777429+Khairajani@users.noreply.github.com>
2025-03-13 02:52:18 +0530

    Time: 0 ms (100%), Space: 21.2 MB (27.76%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -5,15 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def kthSmallest(self, root: TreeNode, k: int) -> int:
-        vector = []
-        def inorder(root):
-            if root:
-                inorder(root.left)
-                vector.append(root.val)
-                inorder(root.right)
-        inorder(root)
-        return vector[k-1]
-                
-            
-            
+        
+    count=0
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        if root==None:
+            return None
+        
+        left = self.kthSmallest(root.left, k)
+        if left is not None:
+            return left
+        self.count+=1
+        if self.count==k:
+            return root.val
+        
+        return self.kthSmallest(root.right,k)
