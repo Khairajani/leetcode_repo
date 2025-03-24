2581ac0f02d2710cadda0f42135bf3ad1f91585d
Himanshu Khairajani <46777429+Khairajani@users.noreply.github.com>
2025-03-25 00:10:58 +0530

    Time: 1451 ms (5.01%), Space: 107.4 MB (50.79%) - LeetHub

---
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
@@ -0,0 +1,53 @@
+class Solution:
+    def create_adjacency_list(self, elements, bidirectional=False):
+        adjacency_list = {}
+        for s, d in elements:
+            # Add edge from source to destination
+            if s in adjacency_list:
+                existing_neigh = adjacency_list[s]
+                existing_neigh.append(d)
+                adjacency_list[s] = existing_neigh
+            else:
+                adjacency_list[s] = [d]
+            
+            # For bidirectional graph, add edge from destination to source
+            if bidirectional:
+                if d in adjacency_list:
+                    existing_neigh = adjacency_list[d]
+                    existing_neigh.append(s)
+                    adjacency_list[d] = existing_neigh
+                else:
+                    adjacency_list[d] = [s]
+        return adjacency_list
+
+    def graph_bfs(self, source):
+        result = []
+        if not self.adjacency_list:
+            return result
+        
+        level_queue = [source]
+        visited_nodes = {source: True}
+
+        while level_queue:
+            non_visited_new_level_nodes = []
+            element_in_current_level = len(level_queue)
+            
+            for i in range(element_in_current_level):
+                curr_element = level_queue.pop(0)
+                non_visited_new_level_nodes.append(curr_element)
+                for neigh in self.adjacency_list.get(curr_element, []):
+                    if not visited_nodes.get(neigh, False):
+                        level_queue.append(neigh)
+                        visited_nodes[neigh] = True  # Mark as visited when adding to queue
+            result.extend(non_visited_new_level_nodes)
+
+        return result
+
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool: 
+
+        self.adjacency_list = self.create_adjacency_list(edges, True)
+        result = self.graph_bfs(source)
+        print(result)
+        if destination in result or source==destination:
+            return True
+        return False
